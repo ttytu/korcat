153216e400faca84db6c2d5e39de528c3a08193f
ttytu <quixote1103@gmail.com>
2023-08-08 17:28:01 +0000

    morpheme inference update

--- backend/apps/files/morpheme/inference.py
+++ backend/apps/files/morpheme/inference.py
@@ -1,4 +1,6 @@
-import os, sys, time
+import os
+import sys
+import time
 import pandas as pd
 import logging
 import torch
@@ -10,57 +12,59 @@ from .data import Multi30k
 from .utils import get_bleu_score, greedy_decode
 from konlpy.tag import Mecab
 
-mecab=Mecab()
+mecab = Mecab()
 
 DATASET = Multi30k()
 
 
 def inference(src):
-    
-    src=mecab.morphs(src)
-    src=" ".join(src)
-    
+    src = mecab.morphs(src)
+    src = " ".join(src)
+
     torch.manual_seed(0)
-    
-    resume_from = "/home/compu/korcat/backend/apps/files/morpheme/chk/1500.pt"
-    
-    #불러오기
-    model = build_model(len(DATASET.vocab_src), len(DATASET.vocab_tgt), device=DEVICE, dr_rate=DROPOUT_RATE)
+
+    resume_from = "/home/compu/korcat/backend/apps/files/morpheme/chk/1400.pt"
+
+    # 불러오기
+    model = build_model(len(DATASET.vocab_src), len(
+        DATASET.vocab_tgt), device=DEVICE, dr_rate=DROPOUT_RATE)
     model.load_state_dict(torch.load(resume_from)["model_state_dict"])
 
-    result=DATASET.translate(model, src, greedy_decode)
-    result=result.replace("<sos>","")
-    result=result.replace("<unk>","")
-    result=result.replace("<eos>","")
-    
-    before=result
-    
-    #사용자 사전 적용
-    user_dict=pd.read_csv("/home/compu/korcat/backend/apps/files/morpheme/user_dic.csv",encoding="utf-8-sig")
-    #중복 제거
-    user_dict=user_dict.drop_duplicates(subset="morp")
-    
-    src_split=src.split(" ")
-    
-    t=result.split(" ")
-    morp_list=list()
-    for idx,item in enumerate(t):
+    result = DATASET.translate(model, src, greedy_decode)
+    result = result.replace("<sos>", "")
+    result = result.replace("<unk>", "")
+    result = result.replace("<eos>", "")
+
+    before = result
+
+    # 사용자 사전 적용
+    user_dict = pd.read_csv(
+        "/home/compu/korcat/backend/apps/files/morpheme/user_dic.csv", encoding="utf-8-sig")
+    # 중복 제거
+    user_dict = user_dict.drop_duplicates(subset="morp")
+
+    src_split = src.split(" ")
+
+    t = result.split(" ")
+    morp_list = list()
+
+    for idx, item in enumerate(t):
         try:
-            tmp=""
-            if item=="/":
-                tmp=t[idx-1]+" "+item+" "+t[idx+1]
+            tmp = ""
+            if item == "/":
+                tmp = t[idx-1]+" "+item+" "+t[idx+1]
                 morp_list.append(tmp)
         except:
             continue
-    
-    for idx,item in enumerate(morp_list):
+
+    for idx, item in enumerate(morp_list):
         try:
-            if item.split(" ")[0]!=src_split[idx] and "V" not in item.split(" ")[2] and "F" not in item.split(" ")[2]:
-                p=user_dict[user_dict.eq(src_split[idx]).any(axis=1)]
-                replace=p["morp"].values[0]+" / "+p["label"].values[0]
-                morp_list[idx]=replace
+            if src_split[idx] not in item:
+                p = user_dict[user_dict.eq(src_split[idx]).any(1)]
+                replace = p["morp"].values[0]+" / "+p["label"].values[0]
+                morp_list[idx] = replace
         except:
             continue
-    
+
     print(result)
     return result
